a51e3701f547136da3aa8d93a01d28bf099a4721
Lankow <maciej.lankow@gmail.com>
2025-05-04 14:02:08 +0200

    Get a grip on hid read write functions.

--- AlientekTest/AlientekDP100.cs
+++ AlientekTest/AlientekDP100.cs
@@ -1,4 +1,5 @@
-﻿using HidSharp;
+﻿using Device.Net;
+using HidSharp;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,6 +17,9 @@ namespace AlientekTest
         public AlientekDP100()
         {
             var devices = DeviceList.Local.GetHidDevices();
+            var deviceList = DeviceList.Local;
+
+            _device = deviceList.GetHidDeviceOrNull(vendorID: VendorId);
 
             foreach (var device in devices)
             {
@@ -30,6 +34,73 @@ namespace AlientekTest
             if (_device == null && !_device.TryOpen(out _stream))
             {
                 throw new InvalidOperationException($"Unable to open {ProductName} device.");
+            }
+
+            Console.WriteLine(_stream == null);        
+        }
+
+        public bool Connect()
+        {
+            return true;
+        }
+        public void Disconnect()
+        {
+        }
+
+        public bool GetVoltageCurrent(out float voltage, out float current)
+        {
+            voltage = float.NaN;
+            current = float.NaN;
+
+            return true;
+        }
+
+        public void SetState(bool state)
+        {
+
+        }
+
+        public void SetVoltage(float voltage)
+        {
+
+        }
+
+        public void GetBasicInfo()
+        {
+            if (_device.TryOpen(out HidStream stream))
+            {
+                var frame = new Frame
+                {
+                    DeviceAddress = 251,
+                    FunctionType = FrameFunctionType.FRAME_BASIC_INFO,
+                    Sequence = 0,
+                    DataLen = 0,
+                    Data = Array.Empty<byte>()
+                };
+
+                var buffer = FrameParser.FrameToBytes(frame);
+
+                stream.Write(buffer);
+                Console.WriteLine("Data written to HID device.");
+
+                int inputReportLength = _device.GetMaxInputReportLength();
+                byte[] inputReport = new byte[inputReportLength];
+
+                try
+                {
+                    int bytesRead = stream.Read(inputReport, 0, inputReport.Length);
+                    Console.WriteLine($"Received {bytesRead} bytes: {BitConverter.ToString(inputReport)}");
+                }
+                catch (TimeoutException)
+                {
+                    Console.WriteLine("Timed out waiting for response.");
+                }
+
+                stream.Close();
+            }
+            else
+            {
+                Console.WriteLine("Could not open HID stream.");
             }
         }
     }
--- AlientekTest/Program.cs
+++ AlientekTest/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace AlientekTest
+﻿using System;
+
+namespace AlientekTest
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             AlientekDP100 alientek = new AlientekDP100();
+
+            Console.WriteLine("START");
+            alientek.GetBasicInfo();
+            Console.WriteLine("STOP");
         }
     }
 }
