ed97985fe2e84894fa29250a7ee8f73936bb8fa7
Lankow <maciej.lankow@gmail.com>
2025-05-17 13:04:41 +0200

    Add wpf app.

---
+++ DP100App/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using Alientek_DP100;
+
+namespace DP100App
+{
+    public partial class MainWindow : Window
+    {
+        private readonly AlientekDP100 _alientek;
+        public MainWindow()
+        {
+            _alientek = new AlientekDP100();
+            InitializeComponent();
+        }
+    }
+}
